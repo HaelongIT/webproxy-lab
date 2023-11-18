6cc1200650ca3043dceb5b3ea4b4ec7a388eefbf
Kyunghyun So <valentine92exp@gmail.com>
2023-11-18 15:24:57 +0000

    fine tune

--- EchoServer/echo.c
+++ EchoServer/echo.c
@@ -10,6 +10,7 @@ void echo(int connfd) {
     //connfd의 변경을 기다림(네트워크 대기)
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0){
         printf("%d byte를 받았습니다\n", (int)n);
+        printf("%s",buf);
         //connfd 변경을 통해 수신.
         Rio_writen(connfd, buf, n);
     }
