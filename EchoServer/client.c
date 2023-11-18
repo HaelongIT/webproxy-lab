f7a5763220bc0a0d49b780e50598f30c9d344911
HaelongIT <soja0529@gmail.com>
2023-11-18 05:42:05 +0000

    클라 첫 커밋

--- EchoServer/client.c
+++ EchoServer/client.c
@@ -1,7 +1,33 @@
 #include "csapp.h"
 #include <stdio.h>
 
-int main(void) {
-    printf("client\n");
+int main(int argc, char **argv) {
+// argc : 인자의 갯수 , argv : 프로그램의 리스트
+    // printf("client\n");
+
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <호스트> <포트>\n", argv[0]);
+        // argv[0] : 프로그램명
+        // stderr에 출력
+        exit(0);
+    }
+    host = argv[1];
+    prot = argv[2];
+
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
+    }
+    Close(clientfd);
+    exit(0);
+
     return 0;
 }
