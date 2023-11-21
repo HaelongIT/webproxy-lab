399b99beaa4f8f1289fa5c206746a0a156cdd727
Kyunghyun So <valentine92exp@gmail.com>
2023-11-21 11:39:19 +0000

    dynamic with template

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,13 +1,20 @@
 #include "csapp.h"
+
+char * replace(
+    char const * const original, 
+    char const * const pattern, 
+    char const * const replacement
+);
+
 int main(void) {
-  char *method, *buf, *p;
+  char *method, *filebuf, *buf, *p;
   char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
   int n1 = 0, n2 = 0;
 
   method = getenv("REQUEST_METHOD");
 
   if ((buf = getenv("QUERY_STRING")) != NULL){
-    if(strcmp(buf,"")){
+    if(buf[0]!='\0'){
       p = strchr(buf,'&');
       *p = '\0';
       strcpy(arg1,buf);
@@ -19,14 +26,33 @@ int main(void) {
     }
   }
 
-  sprintf(content, "<!DOCTYPE html><html lang=ko><head><meta charset=UTF-8><title>Adder</title></head><body><h1>So Dynamic</h1>");
-  if(strcmp(buf,"")){
-    sprintf(content,"%s<p>%d + %d = %d</p>",content,n1,n2,n1+n2);
-  }
-  sprintf(content,"%s<form action=\"/cgi-bin/adder\" method=\"get\">"
-  "<input type=\"tex\" name=\"n1\" /><input type=\"text\" name=\"n2\" />"
-  "<input type=\"submit\" value=\"Submit\" /></form>",content);
-  sprintf(content,"%s</body></html>",content);
+  content[0] = '\0';
+  char num[100];
+  char *temp;
+  int srcfd;
+  srcfd = Open("./cgi-bin/adder.html", O_RDONLY, 0);
+  filebuf = (char *)malloc(MAXLINE);
+  Rio_readn(srcfd,filebuf,MAXLINE);
+  Close(srcfd);
+
+  strcpy(content, filebuf);
+  free(filebuf);
+
+  //첫 번째 replace_substr 호출
+  sprintf(num, "%d", n1);
+  temp = replace(content, &"{@n1}", num);
+  strcpy(content, temp);
+  free(temp);
+
+  sprintf(num, "%d", n2);
+  temp = replace(content, &"{@n2}", num);
+  strcpy(content, temp);
+  free(temp);
+
+  sprintf(num, "%d", n1+n2);
+  temp = replace(content, &"{@n3}", num);
+  strcpy(content, temp);
+  free(temp);
 
   /* Generate the HTTP response*/
   printf("Connection: close\r\n");
@@ -36,8 +62,54 @@ int main(void) {
     printf("%s",content);
   }
   fflush(stdout);
+  
   exit(0);
 }
 
 //todo printf->서버로
-//todo template처럼 만들어보기
+
+char * replace(
+    char const * const original, 
+    char const * const pattern, 
+    char const * const replacement
+) {
+  size_t const replen = strlen(replacement);
+  size_t const patlen = strlen(pattern);
+  size_t const orilen = strlen(original);
+
+  size_t patcnt = 0;
+  const char * oriptr;
+  const char * patloc;
+
+  // find how many times the pattern occurs in the original string
+  for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen)
+  {
+    patcnt++;
+  }
+
+  {
+    // allocate memory for the new string
+    size_t const retlen = orilen + patcnt * (replen - patlen);
+    char * const returned = (char *) malloc( sizeof(char) * (retlen + 1) );
+
+    if (returned != NULL)
+    {
+      // copy the original string, 
+      // replacing all the instances of the pattern
+      char * retptr = returned;
+      for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen)
+      {
+        size_t const skplen = patloc - oriptr;
+        // copy the section until the occurence of the pattern
+        strncpy(retptr, oriptr, skplen);
+        retptr += skplen;
+        // copy the replacement 
+        strncpy(retptr, replacement, replen);
+        retptr += replen;
+      }
+      // copy the rest of the string.
+      strcpy(retptr, oriptr);
+    }
+    return returned;
+  }
+}
