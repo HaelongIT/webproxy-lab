5dff5f347798fd025fe49a10eaf4ccc38ffc89c8
Kyunghyun So <valentine92exp@gmail.com>
2023-11-22 09:33:09 +0000

    함수 정리

--- proxy.c
+++ proxy.c
@@ -10,13 +10,19 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-void doit(int fd);
+void doit(int , char *);
 void read_requesthdrs(rio_t *);
-void extract_host_port_and_suffix(const char *, char *, char *, char *);
 void *thread(void *);
 
+void make_header(char *, char *, char *, char *);
+int parse_uri(char *, char *, char *);
+int parse_handle(int, char *, char *, char *, char *);
+
+void readRequest(int, char *, char *, char *);
 void responseProxy(int, int);
 void requestProxy(int, int, char *);
+void clienterror(int , char *, char*, 
+                  char *, char *); 
 
 int main(int argc, char **argv) {
   int listenfd, *connfdp;
@@ -44,66 +50,92 @@ int main(int argc, char **argv) {
   }
 }
 
+typedef struct cacheObject{
+  int usedCnt;
+  char url[MAXLINE];
+  char *objectbuf;
+  struct cacheObject *next;
+}CacheObject;
+typedef struct cache{
+  int cache_size;
+  CacheObject *cachebuf;
+}Cache;
+
 void *thread(void *vargp)
 {
   int connfd = *((int *)vargp);
   Pthread_detach(pthread_self());
   Free(vargp);
-  doit(connfd);
+
+  static Cache *cachep;
+  cachep = (Cache *)Malloc(sizeof(Cache));
+
+  doit(connfd, cachep);
   Close(connfd);
   return NULL;  
 }
 
-void doit(int clientfd) {
+void doit(int clientfd, char *cachep) {
   int is_static;
   struct stat sbuf;
-  char startbuf[MAXLINE], clientbuf[MAXLINE];
+  char clientbuf[MAXLINE];
   char method[MAXLINE], url[MAXLINE], version[MAXLINE];
   char filename[MAXLINE], cgiargs[MAXLINE];
-  rio_t rio;
 
+  char host[256];
+  char port[6];
   int serverfd;
   
-  printf("\ndoit!\n");
+  printf("\ndo it!\n");
 
-  Rio_readinitb(&rio, clientfd);
-  Rio_readlineb(&rio, startbuf, MAXLINE);
-  printf("Request headers:\n");
-  printf("%s", startbuf);
-  sscanf(startbuf, "%s %s %s", method, url, version);
+  readRequest(clientfd, method, url, version); //첫줄 읽기(GET만)
+  parse_uri(host, port, url); //version 처리필요
+  parse_handle(clientfd, method, host, port, url);//구조체로 묶기
+
+  serverfd = Open_clientfd(host, port);
+  char headerbuf[MAXLINE];
+  make_header(headerbuf, method, host, url);
+  requestProxy(clientfd, serverfd, headerbuf);
+  responseProxy(serverfd, clientfd);
+  
+}
+
+void make_header(char* buf, char* method, char *host, char* uri){
+  sprintf(buf,"%s %s HTTP/1.0", method, uri);
+  sprintf(buf,"%s\nHost:%s\n"
+  "User-Agent:%s\n"
+  "Connection:close\n"
+  "Proxy-Connection:close\r\n\r\n",buf,host,user_agent_hdr);
+  printf("header: %s\n", buf);
+}
+
+int parse_handle(int clientfd, char *method, char *host, char *port, char *url){
+  printf("method: %s\n", method);
+  printf("host:%s port:%s url:%s\n",host, port, url);
 
   if (strcasecmp(method, "GET") && strcasecmp(method,"HEAD")) {
     clienterror(clientfd, method, "501", "Not implemented",
                 "구현되지 않음");
-    return;
+    return -1;
   }
-  
-  char host[256];
-  char port[6];
 
-  parse_uri(host, port, url);
-  
-  char headerbuf[MAXLINE];
-  printf("method: %s\n", method);
-  printf("host:%s port:%s url:%s\n",host, port, url);
-
-  fflush(stdout);
-
+  //자동 포트 배정
   if(port[0]=='\0'){
     strcpy(port,"5000");
   }
 
-  sprintf(headerbuf,"%s %s HTTP/1.0", method, url);
-  sprintf(headerbuf,"%s\nHost:%s\n"
-  "User-Agent:%s\n"
-  "Connection:close\n"
-  "Proxy-Connection:close\r\n\r\n",headerbuf,host,user_agent_hdr);
-  printf("header: %s\n", headerbuf);
+  return 0;
+}
 
-  serverfd = Open_clientfd(host, port);
-  requestProxy(clientfd, serverfd, headerbuf);
-  responseProxy(serverfd, clientfd);
-  
+void readRequest(int clientfd, char *method, char *url, char *version){
+  rio_t rio;
+  char buf[MAXLINE];
+
+  Rio_readinitb(&rio, clientfd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, url, version);
 }
 
 void requestProxy(int clientfd, int serverfd, char *buf){
@@ -120,6 +152,8 @@ void responseProxy(int serverfd, int clientfd){
   printf("서버응답 읽음\n");
   Rio_writen(clientfd, responsebuf, strlen(responsebuf)); //클라이언트로 전송
   printf("클라이언트에 전송\n");
+
+
 }
 
 void read_requesthdrs(rio_t *rp)
@@ -133,7 +167,6 @@ void read_requesthdrs(rio_t *rp)
   }
   return;
 }
-
 //todo : robust하게 변경하기
 int parse_uri(char *server_name, char *server_port, char *uri)
 {
